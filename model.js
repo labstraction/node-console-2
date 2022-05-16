842d784bae133d443e3bfb8d859ae12a53baa531
labstraction <45163263+labstraction@users.noreply.github.com>
2022-05-16 12:47:34 +0200

    prompt

---
+++ model.js
@@ -0,0 +1,136 @@
+class Publication {
+
+
+  constructor(title, publisher, type, price, copies, discount, tax) {
+    this.title = title;
+    this.publisher = publisher;
+    this.type = type;
+    this.price = price;
+    this.copies = copies;
+    this.discount = discount;
+    this.tax = tax;
+  }
+
+  toString() {
+
+    const publicPrice = this.getPublicPrice() > 0 ? this.getPublicPrice().toFixed(2) : 'Non disponibile'
+
+    const pubString = 'Titlo: ' + this.title + '\n' +
+      'Casa Editrice: ' + this.publisher + '\n' +
+      'Genere: ' + this.type + '\n' +
+      'Price: ' + publicPrice + '€' + '\n' +
+      'Copie: ' + this.copies + '\n' +
+      'Sconto: ' + this.discount + '%';
+
+    return pubString;
+
+  }
+
+  // getPublicPriceWithoutTax(){
+  //   const discount = this.price * this.discount / 100;
+  //   const margin = this.price * 0.3;
+  //   const publicPriceWithoutTax = this.price - discount + margin;
+  //   return publicPriceWithoutTax;
+  // }
+
+  getPublicPrice() {
+
+    if (this.price < 0) {
+      return this.price;
+    }
+
+    const discount = this.price * this.discount / 100;
+    const margin = this.price * 0.3;
+    const tax = this.price * this.tax / 100
+    const publicPrice = this.price + tax - discount + margin;
+    const roundedPublicPrice = Publication.round(publicPrice, 2);
+    return roundedPublicPrice;
+  }
+
+  static round(number, decimalPlace) {
+    const roundedString = number.toFixed(decimalPlace);
+    const roundedNumber = parseFloat(roundedString);
+    return roundedNumber;
+  }
+}
+
+class Book extends Publication {
+
+  constructor(title, author, publisher = 'Non disponibile', type = 'Non classificato', price = -1, copies = 0, pages = -1, yop = -1, discount = 0) {
+    super(title, publisher, type, price, copies, discount, 10);
+    this.author = author;
+    this.pages = pages;
+    this.yop = yop;
+  }
+
+  toString() {
+
+    const pages = this.pages > 0 ? this.pages : 'Sconosciute';
+
+    const year = this.yop > 0 ? this.yop : 'Sconosciuto'
+
+    const bookString = super.toString() + '\n' +
+      'Autore: ' + this.author + '\n' +
+      'Pagine: ' + pages + '\n' +
+      'Anno di Pubblicazione: ' + year;
+    return bookString;
+  }
+
+  // getPublicPrice() {
+  //   const publicPriceWithoutTax = super.getPublicPriceWithoutTax();
+  //   const tax = this.price * 0.1;
+  //   const publicPrice = publicPriceWithoutTax + tax;
+  //   const roundedPrice = this.round(publicPrice, 2);
+
+  //   return roundedPrice;
+  // }
+
+}
+
+
+class Magazine extends Publication {
+
+  constructor(title, publisher, release, periodicy, type, price, copies, discount, releaseDate) {
+    super(title, publisher, type, price, copies, discount, 20);
+    this.release = release;
+    this.periodicy = periodicy;
+    this._releaseDate = releaseDate.getTime();
+  }
+
+  get releaseDate() {
+    const date = new Date(this._releaseDate);
+    return date;
+  }
+
+  set releaseDate(value) {
+    const time = value.getTime();
+    this._releaseDate = time;
+  }
+
+  toString() {
+
+    const magazineString = super.toString() + '\n' +
+      'Numero: ' + this.release + '\n' +
+      'Periodicità: ' + this.periodicy + '\n' +
+      'Data di Pubblicazione: ' + this.releaseDate;
+
+    return magazineString;
+
+  }
+
+  // getPublicPrice(){
+
+  //   const publicPriceWithoutTax = super.getPublicPriceWithoutTax();
+  //   const tax = this.price * 0.2;
+  //   const publicPrice = publicPriceWithoutTax + tax;
+  //   const roundedPrice = this.round(publicPrice, 2);
+
+  //   return roundedPrice;
+
+  // }
+
+
+}
+
+exports.Book = Book;
+exports.Magazine = Magazine;
---
+++ script.js
@@ -0,0 +1,75 @@
+const prompt = require('prompt');
+const model = require('./model.js')
+
+const bookArray = [];
+
+console.log('benvenuto in book manager!')
+
+startMenu();
+
+
+function startMenu() {
+  console.log('sono disponibili tre opzioni');
+  console.log('1) aggiungi un libro');
+  console.log('2) lista libri');
+  console.log('3) esci')
+
+  prompt.start();
+
+  const schema = {
+    properties: {
+      selection: {
+        description: 'Seleziona una delle opzioni',
+      }
+    }
+  };
+
+  prompt.get(schema, startMenuManager);
+}
+
+
+function startMenuManager(err, result){
+  if (result.selection === '1') {
+    insertBook();   
+  } else if (result.selection === '2'){
+
+  } else if (result.selection === '3') {
+    console.log('Grazie e a Presto!')
+    process.exit();
+  } else {
+    console.log('selezione non disponibile');
+    startMenu();
+  }
+}
+
+function insertBook() {
+
+  prompt.start();
+
+  const schema = {
+    properties: {
+      title: {
+        description: 'inserisci il titolo',
+      },
+        author: {
+        description: 'inserisci l\'autore',
+      },
+        publisher: {
+        description: 'inserisci la casa editrice',
+      },
+    }
+  };
+
+  prompt.get(schema, insertBookManger);
+  
+}
+
+function insertBookManger(err, result){
+
+  const book = new model.Book(result.title, result.author, result.publisher);
+
+  bookArray.push(book);
+
+  startMenu();
+
+}
